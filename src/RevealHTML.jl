0637f21bb5aff675c3fee678f6bb93f01a20417c
Renee Trochet <rtrochet@gmail.com>
2015-07-10 13:56:07 -0700

    Fixed namespace conflict

--- src/RevealHTML.jl
+++ src/RevealHTML.jl
@@ -5,14 +5,14 @@ using Interact
 
 import Base.writemime
 
-export RevealHTML
+export Revealable
 
-type RevealHTML
+type Revealable
     show::Bool
     html::ASCIIString
 end
 
-function Base.writemime(stream, ::MIME"text/html", x::RevealHTML)
+function Base.writemime(stream, ::MIME"text/html", x::Revealable)
     if x.show
         println(stream, x.html)
     else
