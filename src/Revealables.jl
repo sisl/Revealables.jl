0a11f471f4358c52a9fff8897b8d0b6cb461798b
Renee Trochet <rtrochet@gmail.com>
2015-07-13 15:06:45 -0700

    Added constructors for different #s of arguments

--- src/Revealables.jl
+++ src/Revealables.jl
@@ -8,28 +8,44 @@ import Base.writemime
 export Revealable
 export revealable
 
+
+
 type Revealable
-    show::Bool
-    divclass::ASCIIString
     html::ASCIIString
+    divclass::ASCIIString
+    show::Bool
+end
+
+Revealable(html::ASCIIString, divclass::ASCIIString = "") = Revealable(html, divclass, false)
+
+
+
+function revealable(html::ASCIIString, divclass::ASCIIString, show::Bool)
+    x = Revealable(html, divclass, show)
+    revealable(x)
+end
+
+function revealable(html::ASCIIString, divclass::ASCIIString = "")
+    x = Revealable(html, divclass, false)
+    revealable(x)
 end
 
 function revealable(x::Revealable)
-	@manipulate for n in togglebutton(; label=string("Show/Hide ", uppercase(x.divclass[1]),x.divclass[2:end]), value=x.show, signal=Input(x.show))
+	@manipulate for n in togglebutton(; label=string("Show/Hide", x.divclass == "" ? "" : string(" ", uppercase(x.divclass[1]),x.divclass[2:end])), value=x.show, signal=Input(x.show))
 	    x.show = n
 	    x
 	end
 end
 
-function revealable(show::Bool, divclass::ASCIIString, html::ASCIIString)
-	x = Revealable(show, divclass, html)
-	revealable(x)
-end
 
 
 function Base.writemime(stream, ::MIME"text/html", x::Revealable)
     if x.show
-        println(stream, string("""<div class='""", x.divclass, """'>""", x.html, """</div>"""))
+        if x.class ==""
+            println(stream, string("""<div>""", x.html, """</div>"""))
+        else
+            println(stream, string("""<div class='""", x.divclass, """'>""", x.html, """</div>"""))
+        end
     else
         println(stream, """        
             """
