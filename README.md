234bc50c58464c45d97687e125de0ee8e7ab9f6d
Mykel Kochenderfer <mykel@stanford.edu>
2017-07-26 22:37:06 -0700

    update to 0.6, encode/decode still fail

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # Revealables
 
-This module allows Markdown (and most HTML) to be hidden and revealed by pressing a button in an IJulia notebook. You can also encode and decode the content using a Caesar cipher.
+This module allows Markdown (and most HTML) to be hidden and revealed by pressing a button in an Jupyter notebook. You can also encode and decode the content using a Caesar cipher.
 
 `Revealables` is useful for hiding and revealing answers to practice problems. When you choose, you can give students the password so they can decode the answers.
 
 ## Installation
 ```julia
 Pkg.add("Revealables")
-Pkg.build("Revealables")
 ```
 
-Normal installation requires standard IPython (version 3 or higher) and IJulia installation with default directory structure.
+You will generally want to use Revealables.jl with the [hide_input](http://jupyter-contrib-nbextensions.readthedocs.io/en/latest/nbextensions/hide_input/readme.html) Jupyter extension installed. This extension hides the code cells. Revealables will reveal the output when a button is pressed. If the extension is not installed, then the code will not be hidden. You can unhide the code cells by clicking on the chevron-up icon.
+
+Instructions for installing Jupyter extensions can be found [here](https://github.com/ipython-contrib/jupyter_contrib_nbextensions/wiki/Home-4.x-(Jupyter)). If you are using [anaconda](https://www.continuum.io/downloads), which supplies the [conda](https://conda.io/docs/) command, you can run the following:
+```
+conda install -c conda-forge jupyter_contrib_nbextensions
+```
+Then you must run:
+```
+jupyter contrib nbextension install --user
+jupyter nbextension enable hide_input/main
+```
 
 ## Use
 Hide and show blocks of Markdown through code like this:
@@ -19,4 +28,4 @@ Hide and show blocks of Markdown through code like this:
 revealable("""Any Markdown can go here!""", "Button Label")
 ```
 
-Please see the [example notebook](example/Show and Hide Answers!.ipynb) for more details.
+Please see the [example notebook](doc/example.ipynb) for more detail.
--- src/Revealables.jl
+++ src/Revealables.jl
@@ -3,8 +3,6 @@ module Revealables
 using Interact
 using Reactive
 
-import Base.writemime, Base.display
-
 export Revealable
 export revealable
 export encode!
@@ -14,65 +12,65 @@ export decode
 
 
 type Revealable
-    markdown::ASCIIString
-    label::ASCIIString
+    markdown::String
+    label::String
     show::Bool
     encoded::Bool
 end
 
-Revealable(markdown::ASCIIString, label::ASCIIString = "Show/Hide", show = false, encoded = false) = Revealable(markdown, label, show, encoded)
+Revealable(markdown::String, label::String = "Show/Hide", show = false, encoded = false) = Revealable(markdown, label, show, encoded)
 
-function revealable(markdown::ASCIIString, label::ASCIIString, show::Bool)
+function revealable(markdown::String, label::String, show::Bool)
     x = Revealable(markdown, label, show)
     revealable(x)
 end
 
-function revealable(markdown::ASCIIString, label::ASCIIString = "Show/Hide")
+function revealable(markdown::String, label::String = "Show/Hide")
     x = Revealable(markdown, label, false)
     revealable(x)
 end
 
 function revealable(x::Revealable)
-    @manipulate for n in togglebutton(; label = x.label, value=x.show, signal=Input(x.show))
+    @manipulate for n in togglebutton(; label = x.label, value=x.show, signal=Signal(x.show))
         x.show = n
         x
     end
 end
 
 
-function rot(text::ASCIIString, key::Int)
+function rot(text::String, key::Int)
     map(text) do c
         if 'a' <= c <= 'z'
-            char(mod(c - 'a' + key, 26) + 'a')
+            Char(mod(c - 'a' + key, 26) + 'a')
         elseif 'A' <= c <= 'Z'
-            char(mod(c - 'A' + key, 26) + 'A')
+            Char(mod(c - 'A' + key, 26) + 'A')
         elseif '&' <= c <= '?'
-            char(mod(c - '&' + key, 26) + '&')
+            Char(mod(c - '&' + key, 26) + '&')
         else
             c
         end
     end
 end
 
-function encode!(x::Revealable, password::ASCIIString)
+function encode!(x::Revealable, password::String)
     if x.encoded
         x
     else
-        key = sum(password)
+        key = sum(map(x->convert(Int, x), collect(password)))
         x.markdown = rot(x.markdown, key)
         x.encoded = true
         x
     end
 end
 
-function encode(x::Revealable, password::ASCIIString)
+function encode(x::Revealable, password::String)
     r = Revealable(x.markdown, x.label, x.show)
     encode!(r, password)
 end
 
-function decode!(x::Revealable, password::ASCIIString)
+function decode!(x::Revealable, password::String)
     if x.encoded
-        key = 26 - sum(password)
+        key = 26 - sum(map(x->convert(Int, x), collect(password)))
         x.markdown = x.markdown = rot(x.markdown, key)
         x.encoded = false
         x
@@ -81,23 +79,15 @@ function decode!(x::Revealable, password::ASCIIString)
     end
 end
 
-function decode(x::Revealable, password::ASCIIString)
+function decode(x::Revealable, password::String)
     r = Revealable(x.markdown, x.label, x.show)
     decode!(r, password)
 end
 
 
-function writemime(io::IO, ::MIME"text/markdown", x::Revealable)
+function Base.show(io::IO, ::MIME"text/markdown", x::Revealable)
     if x.show
         println(io, x.markdown)
-    else
-        println(io, "")
-    end
-end
-
-function display(io::IO, x::Revealable)
-    if x.show
-        x
     else
         println(io, "")
     end
